3150b12fc5eca88649634ddc1bd9796c12c53091
Maxwell Born <borncmaxwell@gmail.com>
2024-10-19 17:01:05 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,19 @@
+// Function that receives a callback and calls it
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  // Function that returns a named function
+  function returnsANamedFunction() {
+    function namedFunction() {
+      console.log("I am a named function");
+    }
+    return namedFunction;
+  }
+  
+  // Function that returns an anonymous function
+  function returnsAnAnonymousFunction() {
+    return function() {
+      console.log("I am an anonymous function");
+    };
+  }
